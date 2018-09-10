4c1753c4be3fd181dfa464d1eaf9781f8e1b1f9d
Nico Campbell <Ncampbell21@choate.edu>
2018-09-09 21:47:13 -0400

    Revert "Merge branch 'master' of https://github.com/ncampbell21/Homework"
    This reverts commit 14fc0ccd3c736df9a8572c4462270ea783584cdb, reversing
    changes made to 8aaae2b95f0ffe8abc96bee45b1bd3d0ee069900.

--- Conversation.py
+++ Conversation.py
@@ -2,11 +2,7 @@
 #Due Monday, Sep. 10
 #A simple program that figures out some information regarding what animals you like. After answering a few questions,
 #the program will open a tab in your browser and bring you to an image of an animal you like.
-<<<<<<< HEAD
 #Sources: https://stackoverflow.com/questions/22125114/how-to-insert-links-in-python is where I read about webbrowser, which I imported to automatically open links.
-=======
-#Sources: https://stackoverflow.com/questions/22125114/how-to-insert-links-in-python is where I read about webbrowser, which imported to automatically open links.
->>>>>>> 929da30f7a04114907db15f01c38286773bdad97
 
 #import webbrowser to automatically open dog/cat images
 import webbrowser as web
@@ -17,58 +13,36 @@ print("Hey!")
 name = input("What's your name?\n")
 
 #Dogs or cats
-<<<<<<< HEAD
 DogvCat = input("Hi "+name+", it's great to meet you.\n Do you prefer dogs, cats, or cows?\n")
 
 #Dogs
 if DogvCat == "Dogs":
 	print("I like dogs too!")
-=======
-DogvCat = input("Hi "+name+", it's great to meet you.\n Do you prefer dogs or cats?\n")
-
-#Dogs
-if DogvCat == "Dogs":
-	print("I like dogs too!\n")
->>>>>>> 929da30f7a04114907db15f01c38286773bdad97
 	#big vs small dogs
 	typedog = input("Do you prefer big or small dogs?\n")
 	#big dogs
 	if typedog =="Big":
 		print("Here's a picture of a big dog:")
-<<<<<<< HEAD
 		#opens link
-=======
->>>>>>> 929da30f7a04114907db15f01c38286773bdad97
 		web.open("https://pics.esmemes.com/papa-shoob-mama-shoob-and-their-shooblet-22854788.png")
 	#small dogs
 	else:
 		print("Here's a picture of a small dog:")
-<<<<<<< HEAD
 		#opens link
 		web.open("https://www.crispypompuppies.com/wp-content/uploads/bryce_pomeranian_puppy_03.jpg")
 #Cats
 else if DogvCat == "Cats":
 	print("Cats are pretty cool, but dogs are a bit cooler.")
-=======
-		web.open("https://www.crispypompuppies.com/wp-content/uploads/bryce_pomeranian_puppy_03.jpg")
-#Cats
-else:
-	print("Cats are pretty cool, but dogs are a bit cooler.\n")
->>>>>>> 929da30f7a04114907db15f01c38286773bdad97
 	#big vs small cats
 	typecat = input("Do you prefer big or small cats?\n")
 	#big cats
 	if typecat =="Big":
 		print("Here's a picture of a big cat:")
-<<<<<<< HEAD
 		#opens link
-=======
->>>>>>> 929da30f7a04114907db15f01c38286773bdad97
 		web.open("https://assets3.thrillist.com/v1/image/2683168/size/tmg-article_tall;jpeg_quality=20.jpg")
 	#small cats
 	else:
 		print("Here's a picture of a small cat:")
-<<<<<<< HEAD
 		#opens link
 		web.open("https://i.pinimg.com/originals/3b/ec/06/3bec063ecb6371b24c984b65ce9fdbff.jpg")
 #cows
@@ -87,6 +61,3 @@ else if DogVCat == "Cows":
 		#open link
 		web.open("https://i.redd.it/io2ob4n5b0h01.jpg")
 
-=======
-		web.open("https://i.pinimg.com/originals/3b/ec/06/3bec063ecb6371b24c984b65ce9fdbff.jpg")
->>>>>>> 929da30f7a04114907db15f01c38286773bdad97
