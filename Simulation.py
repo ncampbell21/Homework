d7ffdf80d096b040bc3a185e9fa210970d116d0c
ncampbell21 <43040825+ncampbell21@users.noreply.github.com>
2019-01-08 17:23:59 -0500

    Add files via upload

---
+++ Simulation.py
@@ -0,0 +1,36 @@
+#Wednesday, January 9
+#I initially expect the chances to be 50/50 if you switch, but obviously there's something larger at play if we are doing a simulation of it.
+#I know that originally there's a 1/3 chance that you got it, but if one choice liminated does it make it more likely if you switch? I'm unsure, but
+#i'm excited to see.
+
+
+import random as r 
+
+loss_changed = 0
+wins_changed = 0
+
+#list of prizes
+doors = ["penny","car","penny"]
+
+#simulations
+for x in range(10000):
+	#shuffle the objects
+	r.shuffle(doors)
+	#my  choice
+	choice = r.randint(0,2)
+	#opened door
+	for opened in range(3):
+		#opens door that I haven't opened and is not car
+		if opened == choice or doors[opened] == "car":
+			continue
+	#the one I chose is the car
+	if doors[choice] == "car":
+		loss_changed +=1 
+
+	#the one I could've switched to is the car
+	else:
+		wins_changed +=1
+
+#print results
+print("you lost the car",loss_changed,"times if you changed")
+print("you won the car",wins_changed,"times if you changed")
